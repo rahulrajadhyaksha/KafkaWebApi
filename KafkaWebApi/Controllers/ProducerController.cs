707916ec4c6c84b5a3138a8ced4745cb4c245736
rajad <rajad@DESKTOP-VG3LPDU>
2022-03-31 11:14:14 +0530

    Add project files.

---
+++ KafkaWebApi/Controllers/ProducerController.cs
@@ -0,0 +1,34 @@
+﻿using Confluent.Kafka;
+using KafkaWebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KafkaWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProducerController : ControllerBase
+    {
+        private ProducerConfig _config;
+        public ProducerController(ProducerConfig config)
+        {
+            this._config = config;
+        }
+        [HttpPost("send")]
+        public async Task<ActionResult> Get(string topic, [FromBody]Employee employee)
+        {
+            string serializedEmployee = JsonConvert.SerializeObject(employee);
+            using (var producer=new ProducerBuilder<Null, string>(_config).Build())
+            {
+                await producer.ProduceAsync(topic, new Message<Null, string> { Value = serializedEmployee });
+                producer.Flush(TimeSpan.FromSeconds(10));
+                return Ok(true);
+            }
+        }
+    }
+}
